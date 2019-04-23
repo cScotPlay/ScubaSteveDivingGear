b900d326bd6950de14677dd11d3fb8807572c91f
cScot <cscot@nmd.so>
2019-04-23 01:34:28 -0500

    updated with the addition of wetsuit items

--- src/main/java/com/cscot/scubasteve/ScubaSteve.java
+++ src/main/java/com/cscot/scubasteve/ScubaSteve.java
@@ -1,31 +1,51 @@
 package com.cscot.scubasteve;
 
+import com.cscot.scubasteve.client.renderer.entity.layers.LayerWetsuit;
+import com.cscot.scubasteve.util.itemgroups.ScubaItemGroup;
 import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.Render;
+import net.minecraft.client.renderer.entity.RenderLivingBase;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.entity.RenderPlayer;
+import net.minecraft.client.renderer.entity.layers.LayerRenderer;
+import net.minecraft.client.renderer.entity.model.ModelBase;
+import net.minecraft.entity.item.EntityArmorStand;
 import net.minecraft.init.Blocks;
+import net.minecraft.inventory.EntityEquipmentSlot;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+import net.minecraftforge.fml.event.lifecycle.*;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("scubasteve")
+@Mod(ScubaSteve.MODID)
 public class ScubaSteve
 {
-    // Directly reference a log4j logger.
-    private static final Logger LOGGER = LogManager.getLogger();
+    public static ScubaSteve instance;
+    public static final String MODID = "scubasteve";
 
-    public ScubaSteve() {
+    public static final ItemGroup scubaItemGroup = new ScubaItemGroup();
+
+    // Directly reference a log4j logger.
+    public static Logger LOGGER = LogManager.getLogger(MODID);
+
+    public ScubaSteve()
+    {
+        instance = this;
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
@@ -34,6 +54,10 @@ public class ScubaSteve
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
         // Register the doClientStuff method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        //FMLJavaModLoadingContext.get().getModEventBus().addListener(this::postSetup);
+
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
@@ -65,20 +89,24 @@ public class ScubaSteve
                 collect(Collectors.toList()));
     }
     // You can use SubscribeEvent and let the Event Bus discover methods to call
+
+    @SubscribeEvent
+    public void postSetup(FMLLoadCompleteEvent event) {
+
+        /*RenderManager manager = Minecraft.getInstance().getRenderManager();
+        Map<String, RenderPlayer> renderPlayerMap = manager.getSkinMap();
+        for(RenderPlayer render : renderPlayerMap.values()) {
+            List<LayerRenderer> list = ObfuscationReflectionHelper.getPrivateValue(RenderLivingBase.class, render, "field_177097_h");
+            list.add(new LayerWetsuit(render));
+        }
+        Render<?> render = manager.getEntityClassRenderObject(EntityArmorStand.class);
+        ((RenderLivingBase<?>) render).addLayer(new LayerWetsuit((RenderLivingBase<?>) render) {
+        });*/
+    }
+
     @SubscribeEvent
     public void onServerStarting(FMLServerStartingEvent event) {
         // do something when the server starts
         LOGGER.info("HELLO from server starting");
-    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
     }
 }
