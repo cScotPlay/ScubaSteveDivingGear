34893f40febf95cf590388d86c4adfdeb0081b9c
cScotPlay <cscotplay@gmail.com>
2019-07-08 22:15:11 -0500

    Added to MaskItem class and removed files

--- src/main/java/com/cscot/scubasteve/objects/items/ISnorkelItem.java
+++
@@ -1,9 +0,0 @@
-package com.cscot.scubasteve.objects.items;
-
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
-
-public interface ISnorkelItem
-{
-    boolean isSnorkel(PlayerEntity player, ItemStack stack);
-}
