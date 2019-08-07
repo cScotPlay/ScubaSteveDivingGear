f9728e958daf5069e7f053e349c90a69fe91ea5c
cScotPlay <cscotplay@gmail.com>
2019-08-07 00:16:19 -0500

    Updated variables

--- src/main/java/com/cscot/scubasteve/objects/items/TankItem.java
+++ src/main/java/com/cscot/scubasteve/objects/items/TankItem.java
@@ -7,7 +7,18 @@ import net.minecraft.item.Item;
 
 public class TankItem extends ArmorItem
 {
+    protected final String name;
+    protected final IArmorMaterial material;
+    protected final EquipmentSlotType armorSlot;
+    protected final Boolean maskCheck;
+    protected final Boolean snorkelCheck;
+
     public TankItem(String nameIn, IArmorMaterial materialIn, EquipmentSlotType slot, Boolean isMask, Boolean isSnorkel, Item.Properties builder) {
         super(materialIn, slot, builder);
+        name = nameIn;
+        material = materialIn;
+        armorSlot = slot;
+        maskCheck = isMask;
+        snorkelCheck = isSnorkel;
     }
 }
